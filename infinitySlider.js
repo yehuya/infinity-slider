d2ff3f24f18c01c3c334c8b261aa3453919f7614
yehuda yadid <yehuda@k.co.il>
2016-07-14 15:33:10 +0300

    update init fn and create slide width fn

--- infinitySlider.js
+++ infinitySlider.js
@@ -10,6 +10,7 @@
      * binding dom element with slider
      */
     Object.prototype.Slider = function(options){
+        var elem = this;
         new Slider(options, elem);
     }
 
@@ -26,6 +27,7 @@
 
         //============ TEST
             this.init();
+            this.width();
         //============ TEST
     }
 
@@ -37,10 +39,12 @@
         loop: false,
         infinity: true,
         speed: 500,
+        direction: 'rtl',
         classes: {
-            slides: 'slide',
-            dot: 'dot',
-            container: 'container'
+            root: 'infinity-slider',
+            slides: 'infinity-slider-slide',
+            dot: 'infinity-slider-dot',
+            container: 'infinity-slider-container'
         }
     }
 
@@ -48,13 +52,21 @@
      * slider init 
      * - add slider class
      * - creaete container div
+     * @return this {element, slides, container}
      */
     Slider.prototype.init = function(){
         var options = this.options;
 
+        this.element.classList.add(options.classes.root);
+
         // create container elem   
         var container = document.createElement('div');
             container.classList.add(options.classes.container);
+            container.classList.add(options.direction);
+
+        // create dot main div
+        var dot = document.createElement('div');
+            dot.classList.add(options.classes.dot);
 
         // clone and append parent child into container
         while(this.element.childNodes.length > 0){
@@ -64,12 +76,53 @@
 
             if(child.nodeName != "#text"){
                 clone.classList.add(this.options.classes.slides);
+
+                // create dot for this slide
+                var span = document.createElement('span');
+                dot.appendChild(span);
             }
 
             container.appendChild(clone);
         }
 
-        this.element.appendChild(container);        
+        this.element.appendChild(container);
+        this.element.appendChild(dot);
+        this.slides = this.clean(container); // return clean childern (without '#text')
+        this.container = container;
+    }
+
+    /**
+     * clean element childNode from '#text' 
+     * @param Object (element)
+     * @return Array of Object (childNodes)
+     */
+    Slider.prototype.clean = function(elem){
+        var child = elem.childNodes;
+        var newChild = [];
+
+        for(var i = 0 ; i < child.length ; i++){
+            if(child[i].nodeName != '#text'){
+                newChild.push(child[i]);
+            }
+        }
+
+        return newChild;
+    }
+    
+    /**
+     * set slides width (responsive)
+     * set container width (responsive)
+     */
+    Slider.prototype.width = function(){
+        var length = this.slides.length;
+        var width = 100 / length;
+        var childern = this.slides;
+        this.container.style.width = 100 * length + '%';
+
+        for(var i = 0 ; i < length ; i++){
+            var slide = childern[i];
+            slide.style.width = width + '%';
+        }
     }
 
     /**
