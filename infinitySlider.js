e2f43f185312bea6b3fe850b1239a6bd1e401a60
yehuda yadid <yehuda@k.co.il>
2016-07-14 11:32:32 +0300

    create base file + test.html

---
+++ infinitySlider.js
@@ -0,0 +1,22 @@
+"use strict"
+
+/**
+ * infinity slider plugin 
+ * Author: yehuda yadid
+ */
+
+var Slider = function(){
+
+}
+
+Slider.prototype.options = {
+    DOM: null,
+    dot: true;
+    classes: {
+        slides: 'slide',
+        dot: 'dot',
+        container: 'container',
+    }
+}
+
+
